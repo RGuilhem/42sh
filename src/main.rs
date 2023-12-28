1585d8b77019b754929d9f06aed18d15cfe2a2ed
Guilhem Raux <g.raux@icloud.com>
2023-12-28 14:39:59 +0100

    basic interactive input loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use std::env;
+use std::io;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(args);
+
+    loop {
+        let mut input = String::new();
+        print!("$ ");
+        let _ = io::stdout().flush();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        print!("{}", input);
+    }
 }
